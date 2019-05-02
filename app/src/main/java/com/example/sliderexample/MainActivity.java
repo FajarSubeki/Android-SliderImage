e934ce241834ff57615ddb79430c630a93c26114
Fajar Subeki <fajarsub06@gmail.com>
2019-05-02 12:46:52 +0700

    Slider

---
+++ app/src/main/java/com/example/sliderexample/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.sliderexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import com.example.sliderexample.slider.FragmentSlider;
+import com.example.sliderexample.slider.SliderIndicator;
+import com.example.sliderexample.slider.SliderPagerAdapter;
+import com.example.sliderexample.slider.SliderView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SliderPagerAdapter mAdapter;
+    private SliderIndicator mIndicator;
+
+    private SliderView sliderView;
+    private LinearLayout mLinearLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        sliderView = (SliderView) findViewById(R.id.sliderView);
+        mLinearLayout = (LinearLayout) findViewById(R.id.pagesContainer);
+        setupSlider();
+    }
+
+    private void setupSlider() {
+        sliderView.setDurationScroll(800);
+        List<Fragment> fragments = new ArrayList<>();
+        fragments.add(FragmentSlider.newInstance("http://www.menucool.com/slider/prod/image-slider-1.jpg"));
+        fragments.add(FragmentSlider.newInstance("http://www.menucool.com/slider/prod/image-slider-2.jpg"));
+        fragments.add(FragmentSlider.newInstance("http://www.menucool.com/slider/prod/image-slider-3.jpg"));
+        fragments.add(FragmentSlider.newInstance("http://www.menucool.com/slider/prod/image-slider-4.jpg"));
+
+        mAdapter = new SliderPagerAdapter(getSupportFragmentManager(), fragments);
+        sliderView.setAdapter(mAdapter);
+        mIndicator = new SliderIndicator(this, mLinearLayout, sliderView, R.drawable.indicator_circle);
+        mIndicator.setPageCount(fragments.size());
+        mIndicator.show();
+    }
+}
---
+++ app/src/main/java/com/example/sliderexample/slider/FragmentSlider.java
@@ -0,0 +1,45 @@
+package com.example.sliderexample.slider;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.example.sliderexample.R;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class FragmentSlider extends Fragment {
+
+    private static final String ARG_PARAM1 = "params";
+
+    private String imageUrls;
+
+    public FragmentSlider() {
+    }
+
+    public static FragmentSlider newInstance(String params) {
+        FragmentSlider fragment = new FragmentSlider();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, params);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        imageUrls = getArguments().getString(ARG_PARAM1);
+        View view = inflater.inflate(R.layout.fragment_slider_item, container, false);
+        ImageView img = (ImageView) view.findViewById(R.id.img);
+        Glide.with(getActivity())
+                .load(imageUrls)
+                .placeholder(R.drawable.nature)
+                .into(img);
+        return view;
+    }
+}
